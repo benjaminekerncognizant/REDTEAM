9b0f650ade75d155e77c44de4c61ae3f3e3d1784
brijen1 <bpatel3293@gmail.com>
2022-03-16 04:00:47 +0530

    fix

--- CogniClothJS/src/app/app.module.ts
+++ CogniClothJS/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { CategorylistComponent } from './categorylist/categorylist.component';
 import { CartComponent } from './cart/cart.component';
@@ -12,7 +11,6 @@ import { AccountComponent } from './account/account.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
     ProductlistComponent,
     CategorylistComponent,
     CartComponent,
