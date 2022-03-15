559779f07da976d63dc8382eac13b178c13ab959
brijen1 <bpatel3293@gmail.com>
2022-03-16 03:55:37 +0530

    component setup

--- CogniClothJS/src/app/app.module.ts
+++ CogniClothJS/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { CategorylistComponent } from './categorylist/categorylist.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    ProductlistComponent,
+    CategorylistComponent,
+    CartComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
