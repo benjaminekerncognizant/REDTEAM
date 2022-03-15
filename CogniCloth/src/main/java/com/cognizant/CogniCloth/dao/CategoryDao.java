cbdf60e99b318d197672bb7cb43f616d09a73394
brijen1 <bpatel3293@gmail.com>
2022-03-15 21:57:40 +0530

    CategoryDao

--- CogniCloth/src/main/java/com/cognizant/CogniCloth/dao/CategoryDao.java
+++ CogniCloth/src/main/java/com/cognizant/CogniCloth/dao/CategoryDao.java
@@ -1,18 +1,40 @@
 package main.java.com.cognizant.CogniCloth.dao;
 
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import main.java.com.cognizant.CogniCloth.entityclasses.Category;
 
-@Autowired
+
 public class CategoryDao {
-	SessionFactory sessionfactory = new SessionFactory();
+	@Autowired
+	private SessionFactory sessionfactory;
+	
 	public Category getName(String title)
 	{
+      Session session = this.sessionfactory.getCurrentSession();
 	  String sql="select c.CategoryName from "+Category.class.getName()+"c";
-	  
-	  
-	  
+	  Query<Category> q = session.createQuery(sql,Category.class);
+	  Category category = q.uniqueResult();
+	  return category;
+	}
 	
-
+	public List<Category> categorylist()
+	{
+	  Session session = this.sessionfactory.getCurrentSession();
+	  String sql="select *from"+Category.class.getName();
+	  Query q = session.createQuery(sql,Category.class);
+	  List<Category> catlist = q.list();
+	  return catlist;
+	}	
+	 
+   public void updateCategory(Category category)
+   {
+		  Session session = this.sessionfactory.getCurrentSession();
+		  session.update(category);	 
+   }
 }
