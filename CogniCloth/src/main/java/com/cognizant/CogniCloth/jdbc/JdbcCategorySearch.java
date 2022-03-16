73e8c12ca65132076ce2cf0aa03ef96c4ee050f6
benjiekern <38410280+benjiekern@users.noreply.github.com>
2022-03-16 12:57:04 -0700

    Update JdbcCategorySearch.java

--- CogniCloth/src/main/java/com/cognizant/CogniCloth/jdbc/JdbcCategorySearch.java
+++ CogniCloth/src/main/java/com/cognizant/CogniCloth/jdbc/JdbcCategorySearch.java
@@ -19,27 +19,71 @@ public class JdbcCategorySearch implements CategorySearchDao
 	{
 		try 
 		{    
-		        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cogniclothdb","root","root");
-			String sql = "Select * from categories";
+		        Connection con = DBConnection.getConnection();
 			Statement smt = con.createStatement();
+			String sql = "SELECT * FROM Products p JOIN Category c ON p.categoryID = c.categoryID WHERE c.categoryName LIKE %" + c.getCategoryName() + "%";
 			ResultSet r = smt.executeQuery(sql);
+			StringBuilder sb = new StringBuilder();
 	
 			while (r.next()) {
-				long categoryID=r.getLong("categoryid");
-				c.setCategoryId(categoryID);
-				
-				String categoryName=r.getString("categoryname");
-				c.setCategoryName(categoryName);
+				long categoryID = r.getLong("categoryid");
+				String categoryName = r.getString("categoryName");
+				sb.append(categoryName);
+				sb.append(", ");
 		  	}
+			sb.delete(sb.length()-2, sb.length());
+			System.out.println(sb.toString());
 		  }
-	catch(SQLException e )
+		catch(SQLException e )
 		{
 		  e.printStackTrace();
 		}
 	}
 
-	public void update(Category c){}
+	public void update(Category c) {
+			
+	}
 		
-	public void delete(Category c) {}
+	//Delete any items from selected category + deletes category itself
+	public void delete(Category c) {
+		try 
+		{    
+		        Connection con = DBConnection.getConnection();
+			Statement smt = con.createStatement();
+			String sql = "DELETE * FROM Products p JOIN Category c ON p.categoryID = c.categoryID WHERE c.categoryName LIKE %" + c.getCategoryName() + "%";
+			smt.executeQuery(sql);
+			sql = "DELETE * FROM Category WHERE categoryName LIKE %" + c.getCategoryName() + "%";
+			smt.executeQuery(sql);
+		  }
+		catch(SQLException e )
+		{
+		  e.printStackTrace();
+		}
+	}
+	
+	//Selects all products that are not in the selected category
+	public void remove(Category c) {
+		try 
+		{    
+		        Connection con = DBConnection.getConnection();
+			Statement smt = con.createStatement();
+			String sql = "SELECT * FROM Products p JOIN Category c ON p.categoryID = c.categoryID WHERE c.categoryName NOT LIKE %" + c.getCategoryName() + "%";
+			ResultSet r = smt.executeQuery(sql);
+			StringBuilder sb = new StringBuilder();
+	
+			while (r.next()) {
+				long categoryID = r.getLong("categoryid");
+				String categoryName = r.getString("categoryName");
+				sb.append(categoryName);
+				sb.append(", ");
+		  	}
+			sb.delete(sb.length()-2, sb.length());
+			System.out.println(sb.toString());
+		  }
+		catch(SQLException e )
+		{
+		  e.printStackTrace();
+		}
+	}
 }
  
